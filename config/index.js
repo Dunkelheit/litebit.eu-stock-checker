606ec2fd31cecb294493519c3a92c884d4f9f649
Arturo Martínez <dunkelheit@gmail.com>
2017-12-21 00:59:02 +0100

    feat: enable / disable services

--- config/index.js
+++ config/index.js
@@ -50,10 +50,18 @@ const config = convict({
             env: 'AMQP_PASSWORD'
         },
         queue: {
-            doc: 'Queue that Samantha will be listening to',
-            format: String,
-            default: null,
-            env: 'AMQP_QUEUE'
+            message: {
+                doc: 'Queue used to have Samantha send messages in Slack',
+                format: String,
+                default: null,
+                env: 'AMQP_QUEUE_MESSAGE'
+            },
+            service: {
+                doc: 'Queue used to have Samantha start and stop services',
+                format: String,
+                default: null,
+                env: 'AMQP_QUEUE_SERVICE'
+            }
         },
         retry: {
             maxTries: {
